f0e697c4ef073f9b42af733a8aa38d73bfe6bade
Chen Guodong <chenguodong@huawei.com>
2022-03-29 13:17:48 +0800

    Support MinGW

--- build_install.bat
+++ build_install.bat
@@ -1,3 +1,4 @@
 set INSTALL_PATH=D:\Develop\test_tools\testngpp2_install
-cmake -B build . -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
-cmake --build build --target install
+@rem cmake -B build . -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%  @rem default compiler
+cmake -B build_MinGW . -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH% -G "MinGW Makefiles"
+cmake --build build_MinGW --target install
